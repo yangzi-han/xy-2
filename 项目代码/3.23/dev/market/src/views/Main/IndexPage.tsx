8e1d34b2b1c2ee00246af3749c55256d598f8e63
yangzi-han <2290551709@qq.com>
2020-03-25 10:08:08 +0800

    总结

--- 项目代码/3.23/dev/market/src/views/Main/IndexPage.tsx
+++ 项目代码/3.23/dev/market/src/views/Main/IndexPage.tsx
@@ -1,10 +1,11 @@
 
-
+import styles from '../../scss/home.module.scss'
 import React,{useEffect} from 'react'
 import {connect} from 'react-redux'
 import {bannerAction} from '../../store/actions/home'
 import {RouteComponentProps} from 'react-router'
-
+import '../../../node_modules/swiper/css/swiper.min.css'
+import  Swiper from 'swiper'
 interface StateType{
     banner: Array<{
         image_url: string,
@@ -39,12 +40,30 @@ let IndexPage: React.FC<StateType & DispatchType & RouteComponentProps> = props=
         props.getBanner();
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-
-    return <>{
+    useEffect(()=>{
+         new Swiper ('.swiper-container', {
+            direction: 'vertical', // 垂直切换选项
+            loop: true, // 循环模式选项
+            // 如果需要分页器
+            pagination: {
+              el: '.swiper-pagination',
+            },
+            
+          })
+    },[])
+        
+    return <>
+        <div className={styles.swiperContainer}>
+            <div className="swiper-wrapper">
+        {
         props.banner.map(item=>{
-            return <img key={item.id} src={item.image_url} alt="" />
+            return <div className="swiper-slide" key={item.id}><img src={item.image_url} alt=""/></div>
         })
-    }</>;
+    }
+     <div className="swiper-pagination"></div>
+        </div>
+        </div>
+    </>;
 }
 
 const mapStateToProps = (state: any)=>{
