69220279c3116b62a9f9d6cad0f3fac996cdbc82
longjq <2108319001@qq.com>
2020-03-25 21:41:15 +0800

    3.25算法练习

---
+++ 算法练习/20.3.25/字母转驼峰.js
@@ -0,0 +1,22 @@
+/**
+ * 输入字母变成驼峰形式
+ */
+//方法1
+function getCamelCase(str) {
+    var arr = str.split( '-' );
+    return arr.map( function( item, index ) {
+        console.log(item)  
+        if( index === 0 ){
+            return item;
+        }else{
+            return item.charAt(0).toUpperCase() + item.slice( 1 );
+        }
+    }).join('');
+}
+console.log(getCamelCase("xiao-shuoss-sfff-fe-hguios"))
+//方法2
+// function getCamelCase( str ){
+//     return str.replace( /-([a-z])/g , function( all, i){    // 注意正则中的(),这里可以匹配到  -s 和s
+//         return i.toUpperCase();                        
+//     })
+//  }
---
+++ 算法练习/20.3.25/斐波那契.js
@@ -0,0 +1,27 @@
+// 最简单的做法：递归。
+
+// function fibonacci(n){
+// 　if (n <= 0) {  
+// 　　return 0;
+// 　}
+// 　if (n == 0) {
+// 　　return 1;
+// 　}
+// 　　return fibonacci(n-1) + fibonacci(n-2);
+// }
+
+function fibonacci(n){
+　　let ori = [0,1];
+　　if (n < 2) {
+　　    return ori[n];
+　　};
+　　let fiboOne = 1,fiboTwo = 0,fiboSum = 0;
+　　for (let i = 2; i <= n; i++) {
+    　　fiboSum = fiboOne + fiboTwo;
+    　　fiboTwo = fiboOne;
+    　　fiboOne = fiboSum;
+　　}
+　　return fiboSum;
+　}
+    
+console.log(fibonacci(5));
