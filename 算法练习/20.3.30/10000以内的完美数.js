0532904e120438eda612a88d035bdb5f21bc1544
longjq <2108319001@qq.com>
2020-03-30 20:40:00 +0800

    3.30算法练习

---
+++ 算法练习/20.3.30/10000以内的完美数.js
@@ -0,0 +1,12 @@
+  //   如果一个数恰好等于它的约数之和
+  for(var i=1;i<=10000;i++){
+    var sum=0;
+    for(var y=1;y<i;y++){
+        if(i%y==0){
+            sum+=y;             
+        } 
+    }        
+    if(sum==i){      
+        console.log(i+"&nbsp")
+    }
+}
